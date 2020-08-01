792c9e641dc01a4191c8783c9a4254266dc5433e
scrubele <mankoania@gmail.com>
2020-08-01 20:09:07 +0300

    Refactoring #2

--- Archive/test.py
+++ Archive/test.py
@@ -18,50 +18,53 @@ from cryptography.fernet import Fernet
 
 # from Extracttext_from_logo2 import *
 
-# args = None
-# logger = None
 
 START_TIME = dt.now()
 LOGO = "bmw"
 SERVICE_NAME = "logodetection"
 REGEX_TO_FIND = "bmw"
 
-# SOURCE=config.inbound_GHB_path
 
 # These values coming from the bash file:
-SOURCE = sys.argv[1]
-SOURCE_BKP = sys.argv[2]
-INSERT_TABLE_NAME = "{}".format(sys.argv[3])
+# SOURCE = sys.argv[1]
+# SOURCE_BKP = sys.argv[2]
+# INSERT_TABLE_NAME = "{}".format(sys.argv[3])
 
 
 def setup_argument_parser():
+    """
+        The function for the argument parser setup.
+        Usage:
+            -v: the verbosity flag
+                This works for -vvvv, -vvv, -vv, -v, -v -v , etc.
+                0 - logging.CRITICAL, 1- logging.ERROR, 2- logging.WARNING,3- logging.INFO, 4-logging.DEBUG
+            other env variables:
+                Example:
+                    python test.py --EMAIL_ADDRESS_REPORT=1
+                Use in file as:
+                    print(args.EMAIL_ADDRESS_REPORT) # receive 1
+    :return:
+    """
     parser = argparse.ArgumentParser(prog=__name__)
     parser.add_argument('--verbose', '-v', default=0, action='count')
-    # parser.add_argument('--env_vars', '-e', nargs='+', help='<Required> Set flag', required=True)
-    # parser.add_argument('--EMAIL_ADDRESS_REPORT', action='store')
-    # parser.add_argument('--inbound_GHB_path', action='store')
-    # parser.add_argument('--EMAIL_ADDRESS12_REPORT', action='store')
+    parser.add_argument('--EMAIL_ADDRESS_REPORT', action='store', required=False)
+    # parser.add_argument('--inbound_GHB_path', action='store', required=False)
+    # parser.add_argument('--EMAIL_ADDRESS12_REPORT', action='store', required=False)
     # parser.add_argument('--PASSWORD', action='store',  required=False)
-    # parser.add_argument('--EMAIL_USER', action='store')
-    # parser.add_argument('--EMAIL_HOST', action='store')
-    # parser.add_argument('--AUTOMATE_PYTHON_PATH', action='store')
-    # parser.add_argument('--MYSQL_HOST', action='store')
-    # parser.add_argument('--MYSQL_DATABASE', action='store')
-    # parser.add_argument('--MYSQL_PORT', action='store')
-    # parser.add_argument('--MYSQL_USER', action='store')
-    # parser.add_argument('--MYSQL_PASSWORD', action='store')
+    # parser.add_argument('--EMAIL_USER', action='store', required=False)
+    # parser.add_argument('--EMAIL_HOST', action='store', required=False)
+    # parser.add_argument('--AUTOMATE_PYTHON_PATH', action='store', required=False)
+    # parser.add_argument('--MYSQL_HOST', action='store', required=False)
+    # parser.add_argument('--MYSQL_DATABASE', action='store', required=False)
+    # parser.add_argument('--MYSQL_PORT', action='store', required=False)
+    # parser.add_argument('--MYSQL_USER', action='store', required=False)
+    # parser.add_argument('--MYSQL_PASSWORD', action='store', required=False)
 
-    # parser.add_argument('--output', '-o', choices=['console', 'sqlserver'], default='console',
-    #                     help='Output of the report')
-    # parser.add_argument('--profiles', default='default',
-    #                     help="AWS credentials/configuration profile to use (coma separated)")
-    # parser.add_argument('reports', nargs='+', choices=['eip', 'elb'], help='The report to run')
-    global args
+    # Non necessary, for the access through -e flag:
+    # parser.add_argument('--env_vars', '-e', nargs='+', help='<Required> Set flag', required=True)
+
+    global args  # make args var accessible in the whole file
     args = parser.parse_args()
-
-
-def setup_global_env_vars():
-    pass
 
 
 def setup_logger(level=logging.INFO):
@@ -70,57 +73,51 @@ def setup_logger(level=logging.INFO):
     :level - logger level, can be DEBUG, INFO, etc.
     :return: None
     """
-    global logger
-    extra = {'__name__': 'Super App'}
-
+    global logger  # make logger accessible in the whole file
     logger = logging.getLogger(__name__)
     syslog = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s %(__name__)s : %(message)s')
     syslog.setFormatter(formatter)
-    levels = [logging.WARNING, logging.INFO, logging.DEBUG]
-    level = levels[min(len(levels) - 1, args.verbose)]  # capped to number of levels
+    levels = [logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
+    level = levels[min(len(levels) - 1, args.verbose)]  # capped to the number of levels
     logger.setLevel(level)
     logger.addHandler(syslog)
+    logger = logging.LoggerAdapter(logger, {'__name__': 'Super App'})
+    # Print levels
+    logger.debug("DEBUG level")
+    logger.info("INFO level")
+    logger.warning("WARNING level")
+    logger.error("ERROR level")
+    logger.critical("CRITICAL level")
 
-    logger = logging.LoggerAdapter(logger, extra)
-    logger.debug("a debug message")
-    logger.info("a info message")
-    logger.warning("a warning message")
 
-
-def create_an_email(config):
+def form_email_body(config):
     """
     The function for creating email body
-    :param config - args or config file
+    :param config - environmental vars or the config file
     :return:
         - email_host
         - email_from
         - email_password
         - email_user
         - email_password
-        - msg - an email message (MIMEMultipart object)
+        - message - an email message (MIMEMultipart object)
     """
     email_host = config.EMAIL_HOST
     email_from = config.EMAIL_ADDRESS_REPORT
     email_to = config.EMAIL_ADDRESS12_REPORT
     email_user = config.EMAIL_USER
     email_password = config.PASSWORD
-    msg = MIMEMultipart()
-    msg['From'] = email_from
-    msg['To'] = email_to
-    msg['Subject'] = 'No ECAM Found_' + str(dt.now())
+    message = MIMEMultipart()
+    message['From'] = email_from
+    message['To'] = email_to
+    message['Subject'] = 'No ECAM Found_' + str(dt.now())
     body = 'There is NO EcamSecure found in this image. Please find attached document for more information!'
-    msg.attach(MIMEText(body, 'plain'))
-    return email_host, email_from, email_to, email_password, email_user, email_password, msg
+    message.attach(MIMEText(body, 'plain'))
+    return email_host, email_from, email_to, email_password, email_user, email_password, message
 
 
-def pr1():
-    print('hey')
-    logger.info('hey')
-    print('no')
-
-
-def init_email(file_name):
+def create_email(file_name):
     """
     The function for creating the email message.
     :param file_name - the file name;
@@ -132,21 +129,21 @@ def init_email(file_name):
     msg_part.set_payload(attachment.read())
     encoders.encode_base64(msg_part)
     msg_part.add_header('Content-Disposition', "attachment; filename= " + file_name)
-    msg.attach(msg_part)
-    msg_text = msg.as_string()
+    message.attach(msg_part)
+    msg_text = message.as_string()
     return msg_text
 
 
 def send_email(email_from, email_to, emailText):
     try:
         server = smtplib.SMTP(email_host, 2525)
-        server.START_TIMEtls()
+        server.starttls()
         server.login(email_user, email_password)
         server.sendmail(email_from, email_to.split(','), emailText)
         server.quit()
         logger.info("Success: email has sent!!")
-    except:
-        logger.info("Email failed to send..")
+    except Exception as exception:
+        logger.error("Email failed to send..")
 
 
 def connect_to_MYSQL_db():
@@ -174,7 +171,7 @@ def connect_to_MYSQL_db():
     except mysql.connector.Error as db_connector:
         logger.info(f"Error code: {db_connector.errno}")
         logger.info(f"SQLSTATE value: {db_connector.sqlstate}")
-        logger.info(f"Error message: {db_connector.msg}")
+        logger.info(f"Error message: {db_connector.message}")
         logger.info(f"Error: {db_connector}")
     return database
 
@@ -187,9 +184,14 @@ def insert_into_MYSQL_DB(value, is_logo):
     """
     database = connect_to_MYSQL_db()
     db_cursor = database.cursor()
-    insert_into_table_query = f"INSERT INTO {INSERT_TABLE_NAME} (image_path,isService,image_text,Created_On) VALUES (%s,%s,%s,%s)"
-    insert_into_alarms_query = f'''INSERT INTO alarms(image,type_of_exception,image_text,created_on,location,type_of_services,comments) 
-                VALUES (%s,%s,%s,%s, "{INSERT_TABLE_NAME} ","{SERVICE_NAME}",'NULL')'''
+    insert_into_table_query = f"""
+                                INSERT INTO {INSERT_TABLE_NAME} (image_path,isService,image_text,Created_On)
+                                VALUES (%s,%s,%s,%s)
+                                """
+    insert_into_alarms_query = f'''
+                INSERT INTO alarms(image,type_of_exception,image_text,created_on,location,type_of_services,comments) 
+                VALUES (%s,%s,%s,%s, "{INSERT_TABLE_NAME}","{SERVICE_NAME}",'NULL')
+                '''
     logger.info(f"checking the flag...{is_logo}")
     if is_logo == 'True':
         logger.info(f"{LOGO} insertion")
@@ -224,7 +226,9 @@ def check_number_of_source_files(source_files, max_file_number):
 
 def select_jpg_files(source_files):
     """
-        The function for choosing unique JPG files that don't exist in sourceBkp folder.
+        The function for choosing unique .JPG files that don't exist in sourceBkp folder.
+        :param source_files: the files from the source folder.
+        :return image_sorted_list: the sorted image list
     """
     image_file_names = []
     duplicate_images = []
@@ -247,6 +251,12 @@ def select_jpg_files(source_files):
 
 
 def select_the_first_n_files(image_sorted_list, number_to_select):
+    """
+    The function for selecting the first N files from the sorted list.
+    :param image_sorted_list: a sorted list of the file objects from the source folder
+    :param number_to_select: the number of files to select
+    :return:
+    """
     # x = 0
     # selected_sorted_files = []
     # for file1 in image_sorted_list:
@@ -268,6 +278,12 @@ def select_the_first_n_files(image_sorted_list, number_to_select):
 
 
 def extract_text_from_logo(selected_file, is_logo):
+    """
+        The function for extracting text from files.
+    :param selected_file:
+    :param is_logo:
+    :return:
+    """
     text_from_logo = ""
     try:
         text_from_logo = azure_detec_text(selected_file, SOURCE)
@@ -279,7 +295,7 @@ def extract_text_from_logo(selected_file, is_logo):
         except_value = selected_file,
         except_value = except_value + ("bad data file in " + LOGO, "",)
         insert_into_MYSQL_DB(except_value, is_logo)
-        exception_email_text = init_email(selected_file)
+        exception_email_text = create_email(selected_file)
         send_email(email_from, email_to, exception_email_text)
         shutil.move(selected_file, SOURCE_BKP)
         sys.exit()
@@ -287,6 +303,10 @@ def extract_text_from_logo(selected_file, is_logo):
 
 
 def form_the_data_for_insertion(selected_sorted_files):
+    """
+        The function that prepare data for insertion into the database.
+        It processes images, extracts the text from them.
+    """
     is_logo = False
     data_to_insert_in_db = []
 
@@ -339,7 +359,7 @@ def insert_data_into_db(data_to_insert_in_db):
         #     is_logo_found = LOGO + ' not Found'
         elif is_logo == 'False':
             logger.info(is_logo_found)
-            email_text = init_email(filename)
+            email_text = create_email(filename)
             send_email(email_from, email_to, email_text)
 
         data_to_insert = file_path + (is_logo_found, image_text, final_date_insertion_db,)
@@ -356,17 +376,18 @@ def display_execution_time():
 
 
 def setup_env():
+    """
+        The function that setups argarser and the logger.
+    """
     setup_argument_parser()
     setup_logger()
-    setup_global_env_vars()
-    pr1()
 
 
 def main():
     setup_env()
-    global email_host, email_from, email_to, email_password, email_user, email_password, msg
-    email_host, email_from, email_to, email_password, email_user, email_password, msg = create_an_email(args)
-    print(email_host)
+    global email_host, email_from, email_to, email_password, email_user, email_password, message  # make these vars accessible in the whole file
+    email_host, email_from, email_to, email_password, email_user, email_password, message = form_email_body(args)
+
     time.sleep(5)
 
     max_file_number = 10
